c9d3962f44203ed543fa20437853173c5ed19717
Dzianis Dashkevich <ddashkevich@brightcove.com>
2023-10-21 19:52:28 -0400

    update index

--- packages/demo/src/index.tsx
+++ packages/demo/src/index.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
-import { FullPlaylistParser, ProgressiveParser } from 'web-media-box/hls-parser';
-import { parse } from 'web-media-box/dash-parser';
+import Hls from 'web-media-box/hls-parser';
+import Dash from 'web-media-box/dash-parser';
 import { Player } from 'web-media-box/player';
 import { HlsPipeline } from 'web-media-box/pipelines/mse/hls';
 import { DashPipeline } from 'web-media-box/pipelines/mse/dash';
@@ -8,8 +8,8 @@ import { DashPipeline } from 'web-media-box/pipelines/mse/dash';
 // import * as ReactDOM from 'react-dom/client';
 // import App from './App';
 
-console.log('hls-parser: ', FullPlaylistParser, ProgressiveParser);
-console.log('dash-parser: ', parse);
+console.log('hls-parser: ', Hls);
+console.log('dash-parser: ', Dash);
 console.log('player: ', Player);
 console.log('Hls-pipeline: ', HlsPipeline);
 console.log('Dash-pipeline: ', DashPipeline);
